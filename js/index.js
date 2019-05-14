eeaf58a6955f5d9d29bd5024c1a6475e317ae438
vladmog <vladmog@gmail.com>
2019-05-14 13:37:23 -0700

    added 5/10 event listeners

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // Your code goes here
 
+//======================================================
 
 //Scroll to bottom of page: that's all folks
+
 window.addEventListener('scroll', () => {
     let scrollMax = document.documentElement.scrollHeight - window.innerHeight;
     let scrolled = window.scrollY;
@@ -9,10 +11,41 @@ window.addEventListener('scroll', () => {
         alert("That's all folks!");
     }
 })
+//=====================================================
 
 //Make things disappear on double click
 const website = document.querySelector('body');
 website.addEventListener('dblclick', e => {
     e.target.style.display = "none";
 })
+
+//=======================================================
+
+//Border on mouseover
+
+const imgs = document.querySelectorAll("img");
+imgs.forEach(function(element){
+element.addEventListener('mouseover', e => {
+        console.log("You resized");
+        console.log(e)
+        element.style.border = "solid black 10px";
+    })
+})
+
+//=======================================================
+
+
+website.addEventListener('keydown', (e) => {
+    console.log("key log");
+    e.target.style.backgroundColor = "red";
+})
+
+//=======================================================
+
+website.addEventListener('keyup', (e) => {
+    console.log("key log");
+    e.target.style.backgroundColor = "white";
+})
+
+//======================================================
 
--- stretch-assignment/index.js
+++
@@ -1,9 +0,0 @@
-let window = document.querySelector("window");
-console.log("hello");
-window.addEventListener('scroll', () => {
-    let scrollable = document.documentElement.scrollHeight - window.innerHeight;
-    let scrolled = window.scrollY;
-    if (Math.ceil(scrolled) === scrollable){
-        alert("That's all folks!");
-    }
-})
