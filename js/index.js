3c4c96ac29db69977eef22b916cebf1810236a70
vladmog <vladmog@gmail.com>
2019-05-14 15:09:25 -0700

    MVP

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
 //======================================================
-
+//1
 //Scroll to bottom of page: that's all folks
 
 window.addEventListener('scroll', () => {
@@ -12,7 +12,7 @@ window.addEventListener('scroll', () => {
     }
 })
 //=====================================================
-
+//2
 //Make things disappear on double click
 const website = document.querySelector('body');
 website.addEventListener('dblclick', e => {
@@ -20,20 +20,19 @@ website.addEventListener('dblclick', e => {
 })
 
 //=======================================================
-
+//3
 //Border on mouseover
 
 const imgs = document.querySelectorAll("img");
 imgs.forEach(function(element){
 element.addEventListener('mouseover', e => {
-        console.log("You resized");
-        console.log(e)
         element.style.border = "solid black 10px";
     })
 })
 
 //=======================================================
-
+//4
+//Make background red on key down
 
 website.addEventListener('keydown', (e) => {
     console.log("key log");
@@ -41,6 +40,8 @@ website.addEventListener('keydown', (e) => {
 })
 
 //=======================================================
+//5
+//Return background to white on key up
 
 website.addEventListener('keyup', (e) => {
     console.log("key log");
@@ -48,4 +49,60 @@ website.addEventListener('keyup', (e) => {
 })
 
 //======================================================
+//6
+//Alert on right click
+
+website.addEventListener('contextmenu', e => {
+    alert("OH HE'S ABOUT TO GO INTO DEV MODE!!!");
+})
+
+//=======================================================
+//7
+//Borders on single click
+//stopPropagation()
+
+let mainDiv = document.querySelector(".inverse-content");
+mainDiv.addEventListener('click', e => {
+    e.currentTarget.style.border = "solid yellow 5px";
+})
+
+
+let subDiv = document.querySelector(".inverse-content .text-content");
+subDiv.addEventListener('click', e => {
+    e.stopPropagation();
+    e.currentTarget.style.border = "solid orange 5px";
+})
+
+//========================================================
+//8
+//Page load
+window.addEventListener('load', e => {
+    alert("Page hath loaded");
+})
+
+//========================================================
+//9
+//Resize window alert
+window.addEventListener('resize', e => {
+    console.log("Resize");
+    alert("ANOTHER ANNOYING ALERT: Please don't resize, this page was designed to be fixed!")
+})
+
+//========================================================
+//10
+//Copy alert
+window.addEventListener('copy', e => {
+    alert("Oh he can copy and paste too wooooow");
+})
+
+//========================================================
+
+//preventDefault on Nav
+
+let anchors = document.querySelectorAll("a");
+anchors.forEach(function(element){
+    element.addEventListener('click', e => {
+            e.preventDefault();
+        })
+    })
 
